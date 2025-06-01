2f289432064ac6a55a49b0ad2ac01bc14b4e532f
asahapde <60831459+asahapde@users.noreply.github.com>
2025-05-31 22:51:29 -0400

    no exposed url frontend

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api";
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 // Create a single axios instance
 const api = axios.create({
